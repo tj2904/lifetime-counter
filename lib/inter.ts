3cff5e225fdc7a4df1cbf61bbddb07da64aa39e0
tj2904 <tj2904+github@gmail.com>
2023-07-20 12:25:15 +0100

    updates packages

--- lib/inter.ts
+++ lib/inter.ts
@@ -1,11 +1,11 @@
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 
 const inter = Inter({
-  variable: '--inter-font',
-  display: 'swap',
+  variable: "--inter-font",
+  display: "swap",
   // subsets to preload, more on:
   // https://nextjs.org/docs/basic-features/font-optimization#specifying-a-subset
-  subsets: ['latin'],
+  subsets: ["latin"],
 })
 
 export default inter
